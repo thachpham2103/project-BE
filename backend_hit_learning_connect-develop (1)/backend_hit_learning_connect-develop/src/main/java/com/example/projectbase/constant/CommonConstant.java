82658ac66ac9259fcf173f34c896bb8cacf97c09
unknown <lyngocquyen02102005@gmail.com>
2025-11-30 23:31:03 +0700

    add type document

--- backend_hit_learning_connect-develop (1)/backend_hit_learning_connect-develop/src/main/java/com/example/projectbase/constant/CommonConstant.java
+++ backend_hit_learning_connect-develop (1)/backend_hit_learning_connect-develop/src/main/java/com/example/projectbase/constant/CommonConstant.java
@@ -17,7 +17,7 @@ public class CommonConstant {
   public static final String PATTERN_DATE_TIME = "yyyy-MM-dd HH:mm:ss";
   public static final String PATTERN_DATE = "yyyy-MM-dd";
 
-  public static final String CONTENT_TYPE_DOCUMENT = "txt doc pdf ppt pps xls docx";
+  public static final String CONTENT_TYPE_DOCUMENT = "txt doc pdf ppt pps xlsx xls docx";
   public static final String CONTENT_TYPE_IMAGE = "png jpg jpeg webp gif";
   public static final String CONTENT_TYPE_VIDEO = "mp4 mpg mpe mpeg webm mov m4v";
 
